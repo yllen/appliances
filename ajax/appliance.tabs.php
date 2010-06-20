701000cc899e05353ade62c1b735ffd501ee88c7
remi <remi@38f490e2-107f-4625-a063-1ff3b72b6310>
2010-06-20 16:43:20 +0000

    fix Tickets tab
    git-svn-id: https://forge.glpi-project.org/svn/appliances/trunk@110 38f490e2-107f-4625-a063-1ff3b72b6310

--- ajax/appliance.tabs.php
+++ ajax/appliance.tabs.php
@@ -73,7 +73,7 @@ if ($_POST["id"] >0
          break;
 
       case 6 :
-         showJobListForItem('PluginAppliancesAppliance',$_POST["id"]);
+         Ticket::showListForItem('PluginAppliancesAppliance',$_POST["id"]);
          break;
 
       case 9 :
