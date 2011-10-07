bfcae3cb25a7b4d189380baada91bb34c7666ac6
tsmr <tsmr@38f490e2-107f-4625-a063-1ff3b72b6310>
2011-10-07 07:37:31 +0000

    Add use with certifcates plugin
    git-svn-id: https://forge.glpi-project.org/svn/appliances/trunk@175 38f490e2-107f-4625-a063-1ff3b72b6310

--- setup.php
+++ setup.php
@@ -61,6 +61,10 @@ function plugin_init_appliances() {
    if (class_exists('PluginAccountsAccount')) {
       PluginAccountsAccount::registerType('PluginAppliancesAppliance');
    }
+   
+   if (class_exists('PluginCertificatesCertificate')) {
+      PluginCertificatesCertificate::registerType('PluginAppliancesAppliance');
+   }
 
    // Define the type for which we know how to generate PDF, need :
    $PLUGIN_HOOKS['plugin_pdf']['PluginAppliancesAppliance'] = 'PluginAppliancesAppliancePDF';
