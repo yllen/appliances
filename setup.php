a820592d19ee4484a5a42457d0d74f048237bec0
tsmr <tsmr@38f490e2-107f-4625-a063-1ff3b72b6310>
2011-10-08 11:51:15 +0000

    Add use with domains plugin
    git-svn-id: https://forge.glpi-project.org/svn/appliances/trunk@177 38f490e2-107f-4625-a063-1ff3b72b6310

--- setup.php
+++ setup.php
@@ -69,6 +69,10 @@ function plugin_init_appliances() {
    if (class_exists('PluginDatabasesDatabase')) {
       PluginDatabasesDatabase::registerType('PluginAppliancesAppliance');
    }
+   
+   if (class_exists('PluginDomainsDomain')) {
+      PluginDomainsDomain::registerType('PluginAppliancesAppliance');
+   }
 
    // Define the type for which we know how to generate PDF, need :
    $PLUGIN_HOOKS['plugin_pdf']['PluginAppliancesAppliance'] = 'PluginAppliancesAppliancePDF';
