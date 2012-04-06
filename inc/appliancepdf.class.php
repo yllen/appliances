8842e48a45bca767f8d348a45ea8b00d556cf8f9
remi <remi@38f490e2-107f-4625-a063-1ff3b72b6310>
2012-04-06 18:28:22 +0000

    fix constructor prototype
    git-svn-id: https://forge.glpi-project.org/svn/appliances/trunk@189 38f490e2-107f-4625-a063-1ff3b72b6310

--- inc/appliancepdf.class.php
+++ inc/appliancepdf.class.php
@@ -34,7 +34,7 @@
 class PluginAppliancesAppliancePDF extends PluginPdfCommon {
 
 
-   function __construct(PluginAppliancesAppliance $obj=NULL) {
+   function __construct(CommonGLPI $obj=NULL) {
 
       $this->obj = ($obj ? $obj : new PluginAppliancesAppliance());
    }
