b140084c137b02d71e03f1c5b527df43a91e587a
tsmr <tsmr@38f490e2-107f-4625-a063-1ff3b72b6310>
2012-07-10 16:00:02 +0000

    Using with datainjection correction
    git-svn-id: https://forge.glpi-project.org/svn/appliances/trunk@195 38f490e2-107f-4625-a063-1ff3b72b6310

--- inc/applianceinjection.class.php
+++ inc/applianceinjection.class.php
@@ -36,9 +36,10 @@ if (!defined('GLPI_ROOT')){
    die("Sorry. You can't access directly to this file");
 }
 
-/// Computer class
+/// PluginAppliancesApplianceInjection class
 class PluginAppliancesApplianceInjection extends PluginAppliancesAppliance
    implements PluginDatainjectionInjectionInterface {
+
 
    function __construct() {
       //Needed for getSearchOptions !
@@ -58,82 +59,33 @@ class PluginAppliancesApplianceInjection extends PluginAppliancesAppliance
 
    function getOptions($primary_type = '') {
 
-      $tab = parent::getSearchOptions();
+      $tab = Search::getOptions(get_parent_class($this));
 
       //Specific to location
       $tab[3]['linkfield'] = 'locations_id';
+      //$blacklist = PluginDatainjectionCommonInjectionLib::getBlacklistedOptions();
+      //Remove some options because some fields cannot be imported
+      $notimportable = array(5,9,31,80);
+      $options['ignore_fields'] = $notimportable;
+      $options['displaytype'] = array("dropdown"       => array(2,32,3,8,49,10),
+                                      "user"           => array(6,24),
+                                      "multiline_text" => array(4),
+                                      "date"           => array(9),
+                                      "bool"           => array(11,7));
 
-      //Add linkfield for theses fields : no massive action is allowed in the core, but they can be
-      //imported using the commonlib
-      $add_linkfield = array('comment' => 'comment',
-                             'notepad' => 'notepad');
-
-      foreach ($tab as $id => $tmp) {
-         if (in_array($tmp['field'],$add_linkfield)) {
-            $tab[$id]['linkfield'] = $add_linkfield[$tmp['field']];
-         }
-
-         if (!isset($tmp['linkfield'])) {
-            $tab[$id]['injectable'] = PluginDatainjectionCommonInjectionLib::FIELD_VIRTUAL;
-         }
-         else {
-            $tab[$id]['injectable'] = PluginDatainjectionCommonInjectionLib::FIELD_INJECTABLE;
-         }
-
-         if (isset($tmp['linkfield']) && !isset($tmp['displaytype'])) {
-            $tab[$id]['displaytype'] = 'text';
-          }
-
-          if (isset($tmp['linkfield']) && !isset($tmp['checktype'])) {
-             $tab[$id]['checktype'] = 'text';
-          }
-      }
+      $tab = PluginDatainjectionCommonInjectionLib::addToSearchOptions($tab, $options, $this);
 
       return $tab;
    }
 
 
    /**
-    * Standard method to add an object into glpi
-    * WILL BE INTEGRATED INTO THE CORE IN 0.81
-    *
-    * @param values fields to add into glpi
-    * @param options options used during creation
-    *
-    * @return an array of IDs of newly created objects : for example array(Computer=>1, Networkport=>10)
-    */
-   function addObject($values=array(), $options=array()) {
-
-      $lib = new PluginDatainjectionCommonInjectionLib($this, $values, $options);
-      $lib->addObject();
-      return $lib->getInjectionResults();
-   }
-
-
-   /**
-    * Standard method to update an object into glpi
-    * WILL BE INTEGRATED INTO THE CORE IN 0.81
-    *
-    * @param fields fields to add into glpi
-    * @param options options used during creation
-    *
-    * @return an array of IDs of updated objects : for example array(Computer=>1, Networkport=>10)
-    */
-   function updateObject($values=array(), $options=array()) {
-
-      $lib = new PluginDatainjectionCommonInjectionLib($this,$values,$options);
-      $lib->updateObject();
-      return $lib->getInjectionResults();
-   }
-
-
-   /**
     * Standard method to delete an object into glpi
-    * WILL BE INTEGRATED INTO THE CORE IN 0.81
+    * WILL BE INTEGRATED INTO THE CORE IN 0.80
     *
     * @param fields fields to add into glpi
     * @param options options used during creation
-    */
+   **/
    function deleteObject($values=array(), $options=array()) {
 
       $lib = new PluginDatainjectionCommonInjectionLib($this,$values,$options);
@@ -143,19 +95,21 @@ class PluginAppliancesApplianceInjection extends PluginAppliancesAppliance
 
    /**
     * Standard method to add an object into glpi
-    * WILL BE INTEGRATED INTO THE CORE IN 0.81
+    * WILL BE INTEGRATED INTO THE CORE IN 0.80
     *
     * @param values fields to add into glpi
     * @param options options used during creation
     *
     * @return an array of IDs of newly created objects : for example array(Computer=>1, Networkport=>10)
-    */
+   **/
    function addOrUpdateObject($values=array(), $options=array()) {
+      global $LANG;
 
       $lib = new PluginDatainjectionCommonInjectionLib($this,$values,$options);
       $lib->processAddOrUpdate();
       return $lib->getInjectionResults();
    }
+
 }
 
 ?>
