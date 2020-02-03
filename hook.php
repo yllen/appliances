b56fa7b616a5f4e3ec43eab0d766fb53ead560d1
yllen <yllen@38f490e2-107f-4625-a063-1ff3b72b6310>
2020-02-03 15:55:23 +0000

    update header
    git-svn-id: https://forge.glpi-project.org/svn/appliances/trunk@293 38f490e2-107f-4625-a063-1ff3b72b6310

--- hook.php
+++ hook.php
@@ -21,7 +21,7 @@
 
  @package   appliances
  @author    Xavier CAILLAUD, Remi Collet, Nelly Mahu-Lasson
- @copyright Copyright (c) 2009-2019 Appliances plugin team
+ @copyright Copyright (c) 2009-2020 Appliances plugin team
  @license   AGPL License 3.0 or (at your option) any later version
             http://www.gnu.org/licenses/agpl-3.0-standalone.html
  @link      https://forge.glpi-project.org/projects/appliances
