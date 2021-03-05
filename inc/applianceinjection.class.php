089eeb85eecae615645b426e9fc4cfdb48d4f4fc
yllen <yllen@38f490e2-107f-4625-a063-1ff3b72b6310>
2021-03-05 14:47:01 +0000

    fix #5600
    git-svn-id: https://forge.glpi-project.org/svn/appliances/trunk@313 38f490e2-107f-4625-a063-1ff3b72b6310

--- inc/applianceinjection.class.php
+++ inc/applianceinjection.class.php
@@ -21,7 +21,7 @@
 
  @package   appliances
  @author    Xavier CAILLAUD, Remi Collet, Nelly Mahu-Lasson
- @copyright Copyright (c) 2009-2020 Appliances plugin team
+ @copyright Copyright (c) 2009-2021 Appliances plugin team
  @license   AGPL License 3.0 or (at your option) any later version
             http://www.gnu.org/licenses/agpl-3.0-standalone.html
  @link      https://forge.glpi-project.org/projects/appliances
@@ -40,7 +40,7 @@ class PluginAppliancesApplianceInjection extends PluginAppliancesAppliance
    implements PluginDatainjectionInjectionInterface {
 
 
-   static function getTable() {
+   static function getTable($classname = null) {
 
       $parenttype = get_parent_class();
       return $parenttype::getTable();
